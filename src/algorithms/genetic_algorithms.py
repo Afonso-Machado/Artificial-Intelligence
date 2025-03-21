c2111bddf191c34afb4356895ac5581d4f71a360
AfonsoDomingues17 <afdomingues04@gmail.com>
2025-03-21 23:04:37 +0000

    comment to guide Afonso

--- src/algorithms/genetic_algorithms.py
+++ src/algorithms/genetic_algorithms.py
@@ -1,1 +1,7 @@
 # genetic_algorithms.py
+
+#1. flaten lists
+#2. choose random block of dron1
+#3. complete with dron2 missing products
+#4. check if new solution is valid
+#5. do the same for dron2
--- src/algorithms/tabu_search.py
+++ src/algorithms/tabu_search.py
@@ -1,1 +1,2 @@
 # tabu_search.py
+# fix when neighbor is tabu and does not comply with aspiration criteria need to choose the next best neighbor
